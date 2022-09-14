9544c29695f4300eda5547f740d7239eafaa34fa
Emperordian <dozzy4lov@gmail.com>
2022-09-13 19:16:18 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int num_count;
+
+	num_count = 0;
+
+	while (num_count < 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
+		num_count++;
+	}
+}
+	
