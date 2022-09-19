1709895e5e489cec05b6752744db9989b4052eba
Emperordian <dozzy4lov@gmail.com>
2022-09-19 09:44:17 -0700

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: the string to be checked
+ */
+void _puts(char *str)
+{
+	int n;
+
+	n = 0;
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
