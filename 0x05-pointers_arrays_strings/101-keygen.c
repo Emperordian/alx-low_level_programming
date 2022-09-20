d755910fd900a3dec6c4f517f44d51a7aa3134fa
Emperordian <dozzy4lov@gmail.com>
2022-09-20 02:13:48 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,25 +1,36 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-#include "main.h"
 
 /**
- * main: the entry point to the program
+ * main - Entry point
+ * generates random passwords
  *
- * Return: generated password
+ * Return: 0 (success)
  */
 int main(void)
 {
-	char c;
-	int n;
+	int pass[100];
+	int i, sum, n;
 
-	srand(time(0));
-	while (n <= 2645)
+	sum = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
 	{
-		c = rand() % 128;
-		n = +c;
-		putchar(c);
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	putchar(2772 - n);
 
 	return (0);
 }
