dbd3cc972a2b140b265a5eacc4b24524b5a28237
Emperordian <dozzy4lov@gmail.com>
2022-09-19 21:56:45 -0700

    ease

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+#include "main.h"
+
+/**
+ * main: the entry point to the program
+ *
+ * Return: generated password
+ */
+int main(void)
+{
+	char c;
+	int n;
+
+	srand(time(0));
+	while (n <= 2645)
+	{
+		c = rand() % 128;
+		n = +c;
+		putchar(c);
+	}
+	putchar(2772 - n);
+
+	return (0);
+}
