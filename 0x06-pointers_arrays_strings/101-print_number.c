61e6c3313042542552f4c71e3b64d25e567c4a25
Emperordian <dozzy4lov@gmail.com>
2022-09-22 07:09:12 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,1 +1,23 @@
+#include "main.h"
 
+/**
+ * print_number - prints an integer
+ * @n: the input integer
+ */
+void print_number(int n)
+{
+	unsigned int k;
+
+	k = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		k = -k;
+	}
+	if (k / 10)
+	{
+		print_number(k / 10);
+	}
+	_putchar(k % 10 + '0');
+}
