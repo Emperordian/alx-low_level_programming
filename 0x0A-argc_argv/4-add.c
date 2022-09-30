045e71c8f07f9b94a62d34aa27f3313fc93dbb6a
Emperordian <dozzy4lov@gmail.com>
2022-09-30 12:05:58 -0700

    addition of arguments

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j, k;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < 47 || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
